35662c1f655ff0bee80b730fe8e47a7a232a0e40
DaviGrah2 <davigrah2010@gmail.com>
2026-08-04 16:53:41 -0300

    changes

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,8 +1,8 @@
-const { produto } = require("../models/produto.js");
+const produto = require("../models/produto.js");
 
 class produtoController {
     async index(req, res){
-        const produtos = await produto.findAll();
+        const produtos = await produto.body;
         return res.status(200).json(produtos);
     }
 }
